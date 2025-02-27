892838234aa31d00516d708e06d6debfc4e9e53f
Henry Thompson <marialouise1216@gmail.com>
2025-02-27 21:22:33 +0000

    Day_2

---
+++ helloWorld.py
@@ -0,0 +1,51 @@
+# Day 1 - 30DaysOfPython
+
+print(2 + 3)                  # addition(+)
+print(3 - 1)                  # subtraction(-)
+print(2 * 3)                  # multiplication(*)
+print(3 / 2)                  # division(/)
+print(3 ** 2)                 # exponential(**)
+print(3 % 2)                  # modulus(%)
+print(3 // 2)                 # floor division operator(//)
+
+# Checking data types
+print(type(10))               # int
+print(type(3.14))             # float
+print(type(1 + 3j))           # complex number
+print(type('Ava'))            # string
+print(type([1, 2, 3]))        # list
+print(type({'name': 'Ava'}))  # dictionary
+print(type({9.8, 3.14, 2.7})) # set
+print(type((9.8, 3.14, 2.7))) # tuple
+
+# Write an example for different Python data types
+print(type(True))             # boolean
+
+# Find an Euclidian distance between (2, 3) and (10, 8)
+import math
+
+# Define points
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+# Calculate Eclidean distance using the formula
+distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+# Print result
+print("Euclidean Distance:", distance)
+
+# git repo initialise
+# git branch - if on master do next step
+# git branch -m master main
+
+# else
+# git add .
+# git commit -m "Initial commit"
+# git push -u origin main
+
+# else if already initialised repo do the following
+# git add helloWorld.py
+# git commit -m "day_1"
+# git push
+
+print('See comments for a reminder on how to upload progess to git repo')
