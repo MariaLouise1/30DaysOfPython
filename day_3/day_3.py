c577039120ff6078542d179c9f90465f2cdff568
Henry Thompson <marialouise1216@gmail.com>
2025-02-28 22:22:19 +0000

    Day_3

---
+++ day_3/day_3.py
@@ -0,0 +1,28 @@
+# Day 3 - 30DaysOfPython
+
+# Boolean data type
+
+print(True)
+print(False)
+
+# Arithmetic Operations in Python
+
+#Integers
+print('Addition: '), 1 + 2                # 3
+print('Subtraction: ', 2 - 1)             # 1
+print('Multiplication: ', 2* 3)           # 6
+print('Division: ', 4 / 2)                # 2.0 Division in python results in float
+print('Division:', 6 / 2)                 # 3.0
+print('Division: ', 7 / 2)                # 3.5
+print('Division without the remainder: ', 7 // 2)       # 3, result without float
+print('Division without the remainder: ', 7 // 3)       # 2
+print('Modulus: ', 3 % 2)                 # 1, gives the remainder
+print('Exponentiation: ', 2 ** 3)         # 9 it means 2 * 2 * 2
+
+# Floating Numbers
+print('Floating Point number, PI', 3.14)
+print('Floating Point number, gravity', 9.81)
+
+# Complex Numbers
+print('Complex Number: ', 1 + 1j)
+print('Multiplying Complex Numbers: ', (1 + 1j) * (1 - 1j))
